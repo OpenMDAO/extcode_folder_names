e25deefbd1ae25503344db7af59cfed545188d14
Herb Schilling <hschilling@nasa.gov>
2018-05-10 08:52:06 -0400

    More comments

--- external_code_folder_names_example.py
+++ external_code_folder_names_example.py
@@ -3,37 +3,38 @@
  Also shows how to pass the values for directories to subsystems.
 
  The external code components in this script wrap simple python scripts that are also included
- in this repo. Using simple python scripts was just an easy way to demonstrate this. Normally
- you wouldn't have to wrap Python with Python!
+ in this repo. Using simple python scripts was just an easy way to demonstrate running external
+ code. Normally you wouldn't have to wrap Python with Python!
 
- In practice, you will like be running an executable that has been wrapped. Here are the two Python
- scripts that have been wrapped.
+ In practice, you will likely be running an executable that has been wrapped. Here are the two
+ Python scripts that have been wrapped.
 
     extcode_paraboloid.py
     extcode_square.py
 
-The script runs those scripts while inside specific directories for running the wrapped code.
+This script runs those scripts while inside user defined specific directories for running the
+wrapped code.
 '''
 
 import os
 import numpy as np
 from openmdao.api import Problem, ExternalCodeComp, IndepVarComp, Group
 
-# to get the directory of where this script exists
+# to get the directory where this script exists
 DIRECTORY = os.path.dirname((os.path.abspath(__file__)))
 
 class ParaboloidSquaredGroup(Group):
 
     def initialize(self):
-        # want to be able to pass in the top level directory into the constructor
+        # want to be able to pass in the top level directory into the constructor for this Group
         self.options.declare('toplevel_run_directory', types=str)
 
     def setup(self):
-
         # create and connect inputs
         self.add_subsystem('p1', IndepVarComp('x', 3.0))
         self.add_subsystem('p2', IndepVarComp('y', -4.0))
         # Notice how we pass in value for the top level directory to the subsystems
+        #   so that they can make their own subdirectories to run in
         self.add_subsystem('p',
                            ParaboloidExternalCodeComp(
                                toplevel_run_directory=self.options['toplevel_run_directory']))
@@ -52,7 +53,7 @@ class ParaboloidSquaredGroup(Group):
             pass
 
 class ParaboloidExternalCodeComp(ExternalCodeComp):
-    '''Wrap of code that computes the paraboloid function given x and y'''
+    '''Wrap of external Python code that computes the paraboloid function given x and y'''
     def initialize(self):
         super(ParaboloidExternalCodeComp, self).initialize()
         # want to be able to pass in the top level directory into the constructor
@@ -63,21 +64,21 @@ class ParaboloidExternalCodeComp(ExternalCodeComp):
         self.add_input('y', val=0.0)
         self.add_output('f_xy', val=0.0)
 
-        self.input_file = 'paraboloid_input.dat'
-        self.output_file = 'paraboloid_output.dat'
-
         # providing these is optional; the component will verify that any input
         # files exist before execution and that the output files exist after.
+        self.input_file = 'paraboloid_input.dat'
+        self.output_file = 'paraboloid_output.dat'
         self.options['external_input_files'] = [self.input_file,]
         self.options['external_output_files'] = [self.output_file,]
 
+        # Define the command to run
         self.options['command'] = [
             'python',
             os.path.join(DIRECTORY, 'extcode_paraboloid.py'),
             self.input_file, self.output_file
         ]
 
-        # At setup time set unique folder to run in and create it if need be
+        # At setup time set unique folder to run this external code in, and create it if need be
         self.run_directory = os.path.join(self.options['toplevel_run_directory'], 'paraboloid')
         try:
             os.mkdir(self.run_directory)
@@ -88,17 +89,17 @@ class ParaboloidExternalCodeComp(ExternalCodeComp):
         x = inputs['x']
         y = inputs['y']
 
-        # save the directory so we can go back to it
+        # save the current directory so we can go back to it after running the external code
         startdir = os.getcwd()
 
-        # run in directory unique to this external code
+        # change to the directory where this external code will run
         os.chdir(self.run_directory)
 
         # generate the input file for the paraboloid external code
         with open(self.input_file, 'w') as input_file:
             input_file.write('%.16f\n%.16f\n' % (x,y))
 
-        # the parent compute function actually runs the external code
+        # the parent class compute function actually runs the external code
         super(ParaboloidExternalCodeComp, self).compute(inputs, outputs)
 
         # parse the output file from the external code and set the value of f_xy
@@ -111,7 +112,7 @@ class ParaboloidExternalCodeComp(ExternalCodeComp):
         os.chdir(startdir)
 
 class SquareExternalCodeComp(ExternalCodeComp):
-    '''Wrap of code that computes the square function given x'''
+    '''Wrap of external Python code that computes the square function given x'''
     def initialize(self):
         super(SquareExternalCodeComp, self).initialize()
         # want to be able to pass in the top level directory into the constructor
@@ -121,11 +122,10 @@ class SquareExternalCodeComp(ExternalCodeComp):
         self.add_input('x', val=0.0)
         self.add_output('f_x', val=0.0)
 
-        self.input_file = 'square_input.dat'
-        self.output_file = 'square_output.dat'
-
         # providing these is optional; the component will verify that any input
         # files exist before execution and that the output files exist after.
+        self.input_file = 'square_input.dat'
+        self.output_file = 'square_output.dat'
         self.options['external_input_files'] = [self.input_file,]
         self.options['external_output_files'] = [self.output_file,]
 
@@ -141,7 +141,6 @@ class SquareExternalCodeComp(ExternalCodeComp):
             os.mkdir(self.run_directory)
         except:
             pass
-
 
     def compute(self, inputs, outputs):
         x = inputs['x']
@@ -168,10 +167,12 @@ class SquareExternalCodeComp(ExternalCodeComp):
         # go back to the original directory
         os.chdir(startdir)
 
-
+# Create the top level model using the Group defined above. Set the top level directory
+#   below which each external code will create their own subdirectory
 prob = Problem(ParaboloidSquaredGroup(toplevel_run_directory='/tmp/paraboloid_squared'))
 
 prob.setup()
 prob.run_model()
 
+# just checking to make sure it worked!
 np.testing.assert_almost_equal(prob['s.f_x'], 225.0)
